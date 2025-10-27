4d1166536517b1b9232e27b9354e2d57df4726af
IZT612 <ivan.zamora@iesnervion.es>
2025-10-27 14:42:28 +0100

    update

--- Unidad 7/Proyecto1/obj/Debug/net8.0/Proyecto1.AssemblyInfo.cs
+++ Unidad 7/Proyecto1/obj/Debug/net8.0/Proyecto1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0b88d6b97a08122086502dcef70011eb8f5533d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1400e782474806eda2a8cdece4b3214419fef391")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
