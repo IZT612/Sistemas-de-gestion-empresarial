4cc4d7c69d534bfd8d5da4dc5b290ad7a6e569b8
IZT612 <ivan.zamora@iesnervion.es>
2025-11-19 14:42:03 +0100

    update

---
+++ Unidad 10/Ejercicio2/Data/Database/Connection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Database
+{
+    internal class Connection
+    {
+
+        public static String getConnectionString()
+        {
+            return "server=ivanzamora.database.windows.net;database=PersonasDB;uid=ivan_prueba;pwd=Password123456789!;trustServerCertificate = true;";
+        }
+
+    }
+}
---
+++ Unidad 10/Ejercicio2/Domain/Entities/Persona.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    internal class Persona
+    {
+
+        #region atributos
+        private int _id;
+        private string _nombre;
+        private string _apellidos;
+        private DateTime _fecha;
+        private string _direccion;
+        private string _telefono;
+        #endregion
+
+        #region getters y setters
+
+        public int id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+        public string apellidos
+        {
+            get { return _apellidos; }
+            set { _apellidos = value; }
+        }
+
+        public DateTime fecha
+        {
+            get { return _fecha; }
+            set { _fecha = value; }
+        }
+
+        public string direccion
+        {
+            get { return _direccion; }
+            set { _direccion = value; }
+        }
+
+        public string telefono
+        {
+            get { return _telefono; }
+            set { _telefono = value; }
+        }
+        #endregion
+
+    }
+}
