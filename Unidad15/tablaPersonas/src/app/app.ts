3a91f896113b2a2999c06063bd76d05c7f95bdaf
IZT612 <ivan.zamora@iesnervion.es>
2026-01-14 14:41:06 +0100

    update

---
+++ Unidad15/tablaPersonas/src/app/app.ts
@@ -0,0 +1,15 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TablaPersonas } from './components/tabla-personas/tabla-personas';
+import { FormularioPersona } from './components/formulario-persona/formulario-persona';
+import { ListadoPersonas } from './components/listado-personas/listado-personas';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, TablaPersonas, FormularioPersona,ListadoPersonas],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('holaMundoAngular');
+}
